7994ecf15c91633c311896bf43e46c8203d2df9e
Aditya-Singh714 <adityajaat0000@gmail.com>
2024-07-21 16:40:57 +0530

    implemented Collaborative Editor Room

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,11 +1,11 @@
 "use server";
 
-import { nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
 import { parseStringify } from '../utils';
- 
-export const createDocument = async ({userId, email }: CreateDocumentParams) =>{
+
+export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
     const roomId = nanoid();
 
     try {
@@ -15,21 +15,37 @@ export const createDocument = async ({userId, email }: CreateDocumentParams) =>{
             title: 'Untitled'
         }
 
-        const usersAccesses : RoomAccesses= {
+        const usersAccesses: RoomAccesses = {
             [email]: ['room:write']
         }
 
         const room = await liveblocks.createRoom(roomId, {
-           metadata,
-           usersAccesses,
-           defaultAccesses: []
-          }); 
-          
-          revalidatePath('/');
+            metadata,
+            usersAccesses,
+            defaultAccesses: []
+        });
 
-          return parseStringify(room);
-          
+        revalidatePath('/');
+
+        return parseStringify(room);
+
     } catch (error) {
         console.log(`Error happend while creating a room : ${error}`);
+    }
+}
+
+export const getDocument = async ({ roomId, userId }: { roomId: string, userId: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+        if (!hasAccess) {
+            throw new Error("You do not have access to this document");
+        }
+
+        return parseStringify(room);
+    } catch (error) {
+        console.log(`Error happened while getting a room: ${error}`);
     }
 }
